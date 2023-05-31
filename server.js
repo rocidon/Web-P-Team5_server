ca108215f57cc2829a9f9c109b9a2a33d9163744
3Harang <jungjaeseung9475@gmail.com>
2023-05-31 17:40:19 +0900

    Post 구현
    * 쿼리문 모두 server.js에서 실행되게 수정( CREATE DATABASE, CREATE TABLE)
    * posts table과 comments table에 post하여 DB에 Insert하는 API 구현
    * 게시글/댓글 작성시 uuid 생성하여 부여
    * 현재 시간 받아와 timestamp에 기록

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8080;
+const cors = require("cors");
+const { v4 } = require("uuid");
+
+const uuid = () => {
+  const tokens = v4().split("-");
+  return tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4];
+};
 
 const mysql = require("mysql");
 const con = mysql.createConnection({
@@ -9,19 +16,61 @@ const con = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "1248",
-  database: "webpdb",
+  database: "webp_db",
 });
 
 con.connect(function (err) {
   if (err) throw err;
-  console.log("Connected");
+  console.log("오류 없이 연결 성공~ 오류 뜨면 정재승에게 문의");
+  con.query(
+    "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1248';",
+    function (err, result) {
+      if (err) throw err;
+    }
+  );
+  // 아래 여러줄 주석 돼 있는 부분 최초 실행시 주석풀고 실행해주세요.
+  // webp_db라는 Database 생성하는 쿼리문 입니다.
+  // 1회 실행 이후 다시 주석처리 해주세요.
+  // 더 아래 있는 여러줄 주석이랑 동시에 풀고 하면 안됩니다.
+  /*
+  con.query("CREATE DATABASE webp_db;", function(err,result){
+    if(err) throw err;
+  });
+  */
+
+  // 위에 주석 처리 된거 풀고 1회 실행 한 뒤 아래도 똑같이 실행해주세요.
+  // 최초 실행 이후엔 다시 주석처리 해야 DB 초기화 되지 않습니다~
+  /*
+  const sql_drop_posts_table = "DROP TABLE IF EXISTS posts;";
+  const sql_drop_comments_table = "DROP TABLE IF EXISTS comments;";
+  const sql_create_posts_table =
+    "CREATE TABLE posts (no INT NOT NULL AUTO_INCREMENT, uuid VARCHAR(32) NOT NULL, creator VARCHAR(10) NOT NULL, uid VARCHAR(32) NOT NULL, title VARCHAR(20) NOT NULL, text VARCHAR(200) NOT NULL, likecount INT NOT NULL, timestamp VARCHAR(13) NOT NULL, PRIMARY KEY (no), UNIQUE (uuid));";
+  const sql_create_comments_table =
+    "CREATE TABLE comments ( no INT NOT NULL AUTO_INCREMENT, uuid VARCHAR(32) NOT NULL, uuid2 VARCHAR(32) NOT NULL, creator VARCHAR(10) NOT NULL, uid VARCHAR(32) NOT NULL, text VARCHAR(200) NOT NULL, likecount INT NOT NULL, timestamp VARCHAR(13) NOT NULL, PRIMARY KEY (no), UNIQUE (uuid2));";
+
+
+  con.query(sql_drop_posts_table, function (err, result) {
+    if (err) throw err;
+  });
+  con.query(sql_drop_comments_table, function (err, result) {
+    if (err) throw err;
+  });
+  con.query(sql_create_posts_table, function (err, result) {
+    if (err) throw err;
+  });
+  con.query(sql_create_comments_table, function (err, result) {
+    if (err) throw err;
+  });
+  console.log("posts, comments table 생성 됨");
+  */
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port} !!`));
-
 app.use(express.json());
-var cors = require("cors");
 app.use(cors());
+
+app.listen(port, () =>
+  console.log(`서버 포트 할당 됨~ 사용 포트: http://localhost:${port}`)
+);
 
 app.use(express.static(path.join(__dirname, "web-team-proj-5-front")));
 
@@ -30,13 +79,78 @@ app.get("/", function (request, response) {
     path.join(__dirname, "web-team-proj-5-front/build/index.html")
   );
 });
-app.get(`/posts`, (request, response) => {
-  const sql = `select * from webpdb order by timestamp desc`;
+
+app.get(`/posts`, (req, res) => {
+  const sql = `select * from posts order by timestamp desc;`;
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
-    response.json(result);
+    res.json(result);
   });
 });
+
+app.get("/comments", (req, res) => {
+  const sql = `select * from comments order by timestamp desc;`;
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+app.post("/posts", (req, res) => {
+  let post_uuid = uuid();
+  let post_creator = req.body.params.post_creator;
+  let post_uid = req.body.params.post_uid;
+  let post_title = req.body.params.post_title;
+  let post_text = req.body.params.post_text;
+  let post_likecount = 0;
+  let post_timestamp = new Date().getTime();
+  let values = [
+    post_uuid,
+    post_creator,
+    post_uid,
+    post_title,
+    post_text,
+    post_likecount,
+    post_timestamp,
+  ];
+
+  const sql =
+    "INSERT INTO posts (uuid, creator, uid, title, text, likecount, timestamp) VALUES(?, ?, ?, ?, ?, ?, ?)";
+  con.query(sql, values, (err, result) => {
+    if (err) console.log(err);
+    else res.send(result);
+  });
+  console.log("UUID:" + post_uuid + " 로 게시글 생성됨.");
+});
+
+app.post("/comments", (req, res) => {
+  let post_uuid = req.body.params.post_uuid;
+  let comment_uuid = uuid();
+  let comment_creator = req.body.params.comment_creator;
+  let comment_uid = req.body.params.comment_uid;
+  let comment_text = req.body.params.comment_text;
+  let comment_likecount = 0;
+  let comment_timestamp = new Date().getTime();
+  let values = [
+    post_uuid,
+    comment_uuid,
+    comment_creator,
+    comment_uid,
+    comment_text,
+    comment_likecount,
+    comment_timestamp,
+  ];
+
+  const sql =
+    "INSERT INTO comments (uuid, uuid2, creator, uid, text, likecount, timestamp) VALUES(?, ?, ?, ?, ?, ?, ?)";
+  con.query(sql, values, (err, result) => {
+    if (err) console.log(err);
+    else res.send(result);
+  });
+  console.log("UUID:" + comment_uuid + " 로 댓글 생성됨.");
+});
+
+/*
 
 app.get(`/populars`, (request, response) => {
   const sql = `select * from webpdb order by likecount desc`;
@@ -46,13 +160,8 @@ app.get(`/populars`, (request, response) => {
   });
 });
 
-app.get("/comments", (request, response) => {
-  const sql = `select * from webpdb2`;
-  con.query(sql, function (err, result, fields) {
-    if (err) throw err;
-    response.json(result);
-  });
-});
+
+*/
 
 app.get("*", function (request, response) {
   response.sendFile(
